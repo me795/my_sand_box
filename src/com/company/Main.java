2acb4f665a0d94f2dad9331d790b6c2253820145
sokolovdmvl <user@mail.ru>
2021-07-16 21:28:28 +0300

    Test git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int num = 1;
-	    System.out.println(num);
-	    System.out.println("Hello!");
-		  System.out.println("I'm not your first program!");
+        int num = 1;
+        System.out.println(num);
+        System.out.println("I'm not your first program!");
+        int num2 = 1;
+        System.out.println(num2);
     }
 }
